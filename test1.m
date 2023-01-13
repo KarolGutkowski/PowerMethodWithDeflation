cef078f081f8c0afbc50255858774481189c1cd8
KarolGutkowski <karol.gutkowski.stud@pw.edu.pl>
2023-01-13 12:41:11 +0100

    test1 improvements

--- test1.m
+++ test1.m
@@ -2,18 +2,19 @@ function test1()
 type("test1.txt");
 
 
-A = [1 0 0; 0 2 0; 0 3 0];
-tol = 1e-4;
+A = [1 0 0; 0 2 0; 0 0 3];
+tol = 1e-9;
 
 eigenvalues = P2Z35_KGU_PotNormDef(A,tol);
 fprintf("Macierz 1. :\n");
 disp(A);
 fprintf("Wartości własne macierzy:\n");
 %% ZAMIEŃ NA WLASNE OBLICZENIE WARTOSCI WLASNYCH
-disp(eig(A));
+lambdas = [1;2;3];
+disp(sort(lambdas,"descend"));
 fprintf("Wynik metody:\n");
 disp(eigenvalues);
-
+pause();
 
 A = [30 0 0 0;0 -100 0 0;0 0 17 0; 0 0 0 365];
 
@@ -22,19 +23,32 @@ fprintf("Macierz 2. :\n");
 disp(A);
 fprintf("Wartości własne macierzy:\n");
 %% ZAMIEŃ NA WLASNE OBLICZENIE WARTOSCI WLASNYCH
-disp(eig(A));
+lambdas = [365;-100;30;17];
+disp(lambdas);
 fprintf("Wynik metody:\n");
 disp(eigenvalues);
+pause();
 
+v1 = [1;1;1];
+v2 = [1;0;3];
+v3 = [7;1;1];
+V = [v1 v2 v3];
+D = zeros(3);
+lambda1 = 2; 
+lambda2 = 3; 
+lambda3 = 15;
+D(1,1) = lambda1;
+D(2,2) = lambda2;
+D(3,3) = lambda3;
 
-A = [13 0 1;0 -100 7;1 0 17];
+A = eigenvectorsToMatrixGen(V,D);
 
 eigenvalues = P2Z35_KGU_PotNormDef(A,tol);
 fprintf("Macierz 2. :\n");
 disp(A);
 fprintf("Wartości własne macierzy:\n");
 %% ZAMIEŃ NA WLASNE OBLICZENIE WARTOSCI WLASNYCH
-disp(eig(A));
+disp(sort(diag(D),"descend"));
 fprintf("Wynik metody:\n");
 disp(eigenvalues);
 
